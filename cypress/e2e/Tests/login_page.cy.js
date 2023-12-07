4258634e4575a2fe70e09c5c2d535b7d6973156f
dinesh <dineshsutharxii@gmail.com>
2023-12-07 16:45:54 +0530

    Added beforeEach in e2e.js and login_page.cy.js

--- cypress/e2e/Tests/login_page.cy.js
+++ cypress/e2e/Tests/login_page.cy.js
@@ -2,10 +2,41 @@ import { Login_page } from "../Pages/Login_page";
 
 const login_page = new Login_page()
 
-it('POM Testcase', ()=> {
+//Testcases without describe block
+// it('POM Testcase 1', ()=> {
+
+//     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+//     login_page.enterUsername('Admin')
+//     login_page.enterPassword('admin123')
+//     login_page.clickButton()
+// })
+
+// it('POM Testcase 2', ()=> {
     
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    login_page.enterUsername('Admin')
-    login_page.enterPassword('admin123')
-    login_page.clickButton()
+//     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+//     login_page.enterUsername('Admin')
+//     login_page.enterPassword('admin123')
+//     login_page.clickButton()
+// })
+
+//Testcases  describe block
+
+describe("Testcases in describe block", () =>{
+    beforeEach(() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    })
+
+    it('Testcase 1 in describe', ()=> {
+        login_page.enterUsername('Admin')
+        login_page.enterPassword('admin123')
+        login_page.clickButton()
+        cy.get('.oxd-userdropdown-name').click()
+    })
+    
+    it('Testcase 2 in describe', ()=> {
+        login_page.enterUsername('Administrator')
+        login_page.enterPassword('admin123')
+        login_page.clickButton()
+        cy.get('.oxd-userdropdown-name').click()
+    })
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,7 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+beforeEach(() => {
+    cy.log("Hello from e2e.js")
+})
