a4e902d0a74e435823002c03a84cb566e24d077e
dinesh <dineshsutharxii@gmail.com>
2023-12-10 15:43:59 +0530

    Added more Added more testcases

--- cypress/e2e/LocatorDemo.cy.js
+++ cypress/e2e/LocatorDemo.cy.js
@@ -4,12 +4,12 @@ describe('Css Selectors', () => {
     //     cy.viewport(1280, 720)
     // })
 
-    // it('Css Selector With id', () => {
+    it('Css Selector With id', () => {
 
-    //     cy.visit('https://practice.automationtesting.in/')
-    //     cy.get('#menu-icon').click()
-    //     cy.get('#menu-item-224').click()
-    //     })
+        cy.visit('https://practice.automationtesting.in/')
+        cy.get('#menu-icon').click()
+        cy.get('#menu-item-224').click()
+        })
 
     beforeEach(() => {
 
@@ -36,6 +36,15 @@ describe('Css Selectors', () => {
         cy.get('#password').type('adg.sfdfd@gmail.com')
         cy.get(':nth-child(3) > .woocommerce-Button').click()
         cy.get('.woocommerce-MyAccount-navigation-link--customer-logout').should('be.visible')
+
+    })
+
+    it(' Home Page with three Sliders only', () => {
+
+        cy.get('#menu-icon').click()
+        cy.get('#menu-item-40').click()
+        cy.get('#content > nav > a').click()
+        cy.get('#n2-ss-6 > div.n2-ss-slider-1.n2-grab > div > div > div.n2-ss-slide.n2-ss-canvas.n2-ss-slide').should('have.length', 3)
 
     })
 
